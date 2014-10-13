1a8ff1dec44dc963f3bfc4781177364aa137c650
Celso Fernandes <fernandes@zertico.com>
2014-10-13 14:15:18 -0300

    Update oh my zsh install script url

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,12 @@
 # This is a placeholder class.
 class ohmyzsh {
 
-  $curl = 'curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh'
+  $curl = 'curl -L http://install.ohmyz.sh'
   $sh = 'sh'
   $home = "/Users/${::boxen_user}"
 
   exec { 'install oh-my-zsh':
   command => "${curl} | ${sh}",
-  onlyif => "test ! -d ${home}/.oh-my-zsh",
+  onlyif  => "test ! -d ${home}/.oh-my-zsh",
   }
 }
