d47c7d3a5b063de54912c83fbbedab62039c3dee
Celso Fernandes <fernandes@zertico.com>
2014-10-13 14:02:34 -0300

    Only run install zsh command if not installed yet

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,8 +3,10 @@ class ohmyzsh {
 
   $curl = 'curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh'
   $sh = 'sh'
+  $home = "/Users/${::boxen_user}"
 
   exec { 'install oh-my-zsh':
-    command => "${curl} | ${sh}"
+  command => "${curl} | ${sh}",
+  onlyif => "test ! -d ${home}/.oh-my-zsh",
   }
 }
--- spec/classes/ohmyzsh_spec.rb
+++ spec/classes/ohmyzsh_spec.rb
@@ -3,9 +3,12 @@ require 'spec_helper'
 # Check other boxen modules for examples
 # or read http://rspec-puppet.com/tutorial/
 describe 'ohmyzsh' do
+  let(:facts) { {:boxen_user => 'boxen_user'} }
+  it { should contain_class('ohmyzsh') }
   it do
     should contain_exec('install oh-my-zsh').with({
-      :command => 'curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh'
+      :command => 'curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh',
+      :onlyif => "test ! -d /Users/boxen_user/.oh-my-zsh",
     })
   end
 end
